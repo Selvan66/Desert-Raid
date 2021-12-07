c1c8b896e67a093a38514e2644db4d9def7b41e5
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2021-12-07 14:27:13 +0100

    Game and Title states

---
+++ code/GameState.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "State.hpp"
+#include "World.hpp"
+#include "Player.hpp"
+
+class GameState : public State {
+    public:
+        GameState(StateStack& stack, Context context);
+        virtual void draw();
+        virtual bool update(sf::Time dt);
+        virtual bool handleEvent(const sf::Event& event);
+    private:
+        World mWorld;
+        Player& mPlayer;
+};
+
+GameState::GameState(StateStack& stack, Context context)
+: State(stack, context), mWorld(*context.window), mPlayer(*context.player) {
+}
+
+void GameState::draw() {
+    mWorld.draw();
+}
+
+bool GameState::update(sf::Time dt) {
+    mWorld.update(dt);
+
+    CommandQueue& commands = mWorld.getCommandQueue();
+    mPlayer.handleRealtimeInput(commands);
+
+    return true;
+}
+
+bool GameState::handleEvent(const sf::Event& event) {
+    CommandQueue& commands = mWorld.getCommandQueue();
+    mPlayer.handleEvent(event, commands);
+    if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
+        requestStackPush(States::Pause);
+    return true;
+}
---
+++ code/TitleState.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "State.hpp"
+#include "Utility.hpp"
+
+class TitleState : public State {
+    public:
+        TitleState(StateStack& stack, Context context);
+        virtual void draw();
+        virtual bool update(sf::Time dt);
+        virtual bool handleEvent(const sf::Event& event);
+    private:
+        sf::Sprite mBackgroundSprite;
+        sf::Text mText;
+        bool mShowText;
+        sf::Time mTextEffectTime;
+};
+
+TitleState::TitleState(StateStack& stack, Context context) 
+: State(stack, context), mText(), mShowText(true), mTextEffectTime(sf::Time::Zero) {
+    mBackgroundSprite.setTexture(context.textures->get(Textures::TitleScreen));
+    mText.setFont(context.fonts->get(Fonts::Sansation));
+    mText.setString("Press any key to start");
+    centerOrigin(mText);
+    mText.setPosition(context.window->getView().getSize() / 2.f);
+}
+
+void TitleState::draw() {
+    auto& window = *State::getContext().window;
+    window.draw(mBackgroundSprite);
+    if (mShowText)
+        window.draw(mText);
+}
+
+bool TitleState::update(sf::Time dt) {
+    mTextEffectTime += dt;
+    if (mTextEffectTime >= sf::second(0.5f)) {
+        mShowText = !mShowText;
+        mTextEffectTime = sf::Time::Zero;
+    }
+    return true;
+}
+
+bool TitleState::handleEvent(const sf::Event& event) {
+    if (event.type == sf::Event::KeyPressed) {
+        requestStackPop();
+        requestStackPus(States::Menu);
+    }
+    return true;
+}
+
+
