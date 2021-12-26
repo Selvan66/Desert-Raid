ab9fd2de6938cbb8d4a8eab986baf05a9834fce4
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2021-12-26 14:47:04 +0100

    definition of entity

--- code/Game/Entity.hpp
+++ code/Game/Entity.hpp
@@ -22,6 +22,10 @@ class Entity : public SceneNode {
         int mHitpoints;
 };
 
+Entity::Entity(int hitpoints) 
+: mVelocity(), mHitpoints(hitpoints) {
+}
+
 void Entity::setVelocity(sf::Vector2f velocity) {
     mVelocity = velocity;
 }
@@ -41,6 +45,28 @@ void Entity::accelerate(float vx, float vy) {
 
 sf::Vector2f Entity::getVelocity() const {
     return mVelocity;
+}
+
+int Entity::getHitpoints() const {
+    return mHitpoints;
+}
+
+void Entity::repair(int points) {
+    assert(points > 0);
+    mHitpoints += points;
+}
+
+void Entity::damage(int points) {
+    assert(points > 0);
+    mHitpoints -= points;
+}
+
+void Entity::destroy() {
+    mHitpoints = 0;
+}
+
+bool Entity::isDestroyed() const {
+    return mHitpoints <= 0;
 }
 
 void Entity::updateCurrent(sf::Time dt) {
