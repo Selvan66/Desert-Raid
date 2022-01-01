a9cf179bd6075b2dfad5c6a9dbb36aebaf65adac
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-01-01 18:18:31 +0100

    definition of projectile

--- code/Objects/Projectile.hpp
+++ code/Objects/Projectile.hpp
@@ -2,6 +2,21 @@
 
 #include "Objects/Entity.hpp"
 #include "Utils/ResourceIdentifiers.hpp"
+#include "Utils/Utility.hpp"
+
+#include <vector>
+
+struct ProjectileData {
+    int damage;
+    float speed;
+    Textures::ID texture;
+};
+
+std::vector<ProjectileData> initializeProjectileData();
+
+namespace {
+    const std::vector<ProjectileData> Table = initializeProjectileData();
+}
 
 class Projectile : public Entity {
     public:
@@ -20,10 +35,82 @@ class Projectile : public Entity {
         float getMaxSpeed() const;
         int getDamage() const;
     private:
-        virtual void updateCurrent(sf::Time, dt, CommandQueue& commands);
+        virtual void updateCurrent(sf::Time dt, CommandQueue& commands);
         virtual void drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const;
     private:
         Type mType;
         sf::Sprite mSprite;
         sf::Vector2f mTargetDirection;
 };
+
+std::vector<ProjectileData> initializeProjectileData() {
+    std::vector<ProjectileData> data(Projectile::TypeCount);
+
+    data[Projectile::AlliedBullet].damage = 10;
+    data[Projectile::AlliedBullet].speed = 300.f;
+    data[Projectile::AlliedBullet].texture = Textures::Bullet;
+
+    data[Projectile::EnemyBullet].damage = 10;
+    data[Projectile::EnemyBullet].speed = 300.f;
+    data[Projectile::EnemyBullet].texture = Textures::Bullet;
+
+    data[Projectile::Missile].damage = 200;
+    data[Projectile::Missile].speed = 150.f;
+    data[Projectile::Missile].texture = Textures::Missile;
+
+    return data;
+}
+
+Projectile::Projectile(Type type, const TextureHolder& texutres) 
+: Entity(1), mType(type), mSprite(textures.get(Table[type].texture)), mTargetDirection() {
+    centerOrigin(mSprite);
+}
+
+void Projectile::guideTowards(sf::Vector2f position) {
+    assert(isGuided());
+    mTargetDirection = unitVector(position - SceneNode::getWorldPosition());
+}
+
+bool Projectile::isGuided() const {
+    return mType == Missile;
+}
+
+unsigned int Projectile::getCategory() const {
+    if (mType == EnemyBullet) {
+        return Category::EnemyProjectile;
+    }
+    else {
+        return Category::AlliedProjectile;
+    }
+}
+
+sf::FloatRect Projectile::getBoundingRect() const {
+    return SceneNode::getWorldTransform().transformRect(mSprite.getGlobalBounds());
+}
+
+float Projectile::getMaxSpeed() const {
+    return Table[mType].speed;
+}
+
+int Projectile::getDamage() const {
+    return Table[mType].damage;
+}
+
+void Projectile::updateCurrent(sf::Time dt, CommandQueue& commands) {
+    if (isGuided()) {
+        const float approachRate = 200.f;
+
+        sf::Vector2f newVelocity = unitVector(approachRate * dt.asSeconds() * mTargetDirection + Entity::getVelocity());
+        newVelocity *= getMaxSpeed();
+        float angle = std::atan2(newVelocity.y, newVelocity.x);
+
+        Transformable::setRotation(toDegree(angle) + 90.f);
+        Entity::setVelocity(newVelocity);
+    }
+
+    Entity::updateCurrent(dt, commands);
+}
+
+void Projectile::drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const {
+    target.draw(mSprite, states);
+}
