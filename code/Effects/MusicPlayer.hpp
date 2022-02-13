fbcbed028ba264c3ab1af932b90b8e56550fc75f
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-02-13 14:41:41 +0100

    MusicPlayer definition

--- code/Effects/MusicPlayer.hpp
+++ code/Effects/MusicPlayer.hpp
@@ -17,6 +17,43 @@ class MusicPlayer : private sf::NonCopyable {
         void setVolume(float volume);
     private:
         sf::Music mMusic;
-        std::map<Music::ID, std::string> mFilename;
+        std::map<Music::ID, std::string> mFilenames;
         float mVolume;
 };
+
+MusicPlayer::MusicPlayer() 
+: mMusic()
+, mFilenames()
+, mVolume(100.f) {
+    mFilenames[Music::MenuTheme] = "../assets/Music/MenuTheme.ogg";
+    mFilenames[Music::MissionTheme] = "../assets/Music/MissionTheme.ogg";
+}
+
+void MusicPlayer::play(Music::ID theme) {
+    std::string filename = mFilenames[theme];
+
+    if (!mMusic.openFromFile(filename)) {
+        throw std::runtime_error("Music " + filename + " could not be loaded");
+    }
+
+    mMusic.setVolume(mVolume);
+    mMusic.setLoop(true);
+    mMusic.play();
+}
+
+void MusicPlayer::stop() {
+    mMusic.stop();
+}
+
+void MusicPlayer::setPaused(bool paused) {
+    if (paused) {
+        mMusic.pause();
+    } 
+    else {
+        mMusic.play();
+    }
+}
+
+void MusicPlayer::setVolume(float volume) {
+    mVolume = volume;
+}
