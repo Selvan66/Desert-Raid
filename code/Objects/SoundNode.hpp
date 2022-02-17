fd22f0b61a10cd0d8c489abc2793999c0d39e534
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-02-17 10:59:53 +0100

    SoundNode definition

--- code/Objects/SoundNode.hpp
+++ code/Objects/SoundNode.hpp
@@ -12,3 +12,17 @@ class SoundNode : public SceneNode {
     private:
         SoundPlayer& mSounds;
 };
+
+SoundNode::SoundNode(SoundPlayer& player)
+: SceneNode()
+, mSounds(player)
+{
+}
+
+void SoundNode::playSound(SoundEffect::ID sound, sf::Vector2f position) {
+    mSounds.play(sound, position);
+}
+
+unsigned int SoundNode::getCategory() const {
+    return Categoty::SoundEffect;
+}
